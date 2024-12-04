0d3b7e707bfdc3514c695ee362dd14e4bf95fcf3
Erich Wood <gwoody@gmail.com>
2024-12-04 18:56:40 -0500

    Fixed package issue.

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/outblock/flow-swift.git", .exact("0.3.1")),
         .package(url: "https://github.com/daltoniam/Starscream", .exact("4.0.6")),
-        .package(name: "AppKit", url: "https://github.com/reown-com/reown-swift", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/reown-com/reown-swift", from: "1.0.0"),
         .package(url: "https://github.com/1024jp/GzipSwift", .exact("5.2.0")),
     ],
     targets: [
@@ -26,7 +26,7 @@ let package = Package(
             dependencies: [
                 .product(name: "Flow", package: "flow-swift"),
                 .product(name: "Starscream", package: "Starscream"),
-                .product(name: "AppKit", package: "AppKit"),
+                .product(name: "ReownAppKit", package: "reown-swift"),
                 .product(name: "Gzip", package: "GzipSwift"),
             ],
             path: "Sources"
